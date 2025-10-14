d90660ca9c582d2886718ce5d8c230cd2713f779
BumpVersion Action <bumpversion@github-actions>
2025-10-14 18:30:42 +0000

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../demos'))
 project = 'Python Research Toolkit - Simulation'
 copyright = '2025, Gavin Strunk'
 author = 'Gavin Strunk'
-version = "0.1.0"
+version = "0.1.1"
 release = version
 
 # -- General configuration ---------------------------------------------------
